4caaeefb04e558fa7f0f223e3f5e10d2819a6f84
Amruta Sahu <samrutap146@gmail.com>
2024-10-08 18:21:08 +0530

    Added a New Message in Main.java

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ package prac6;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
+        System.out.println("Hello, Java and Git!");
     }
 }
